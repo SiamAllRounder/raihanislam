302676de73f80caf0f07a9e2c2f4916848b1cf3e
siam <seum.bdit@gmail.com>
2023-09-23 09:52:00 +0600

    sign admin setup first done

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('master.admin.dashboard.index3');
+});
 Route::get('/blog', function () {
     return view('blog');
 })->name('blog');
