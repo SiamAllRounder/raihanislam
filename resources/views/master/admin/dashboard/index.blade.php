36b9ca958c87580c89d3d4dc12d0a79438aa767e
siam <seum.bdit@gmail.com>
2023-09-26 17:09:38 +0600

    search box btn fixed

--- resources/views/master/admin/dashboard/index.blade.php
+++ resources/views/master/admin/dashboard/index.blade.php
@@ -44,16 +44,23 @@
   <body>
 
     <!-- ########## START: LEFT PANEL ########## -->
+    <style>
+      .btn, .sp-container button {
+padding: 0.77rem 0.75rem;
+}
+    </style>
     <div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> starlight</a></div>
     <div class="sl-sideleft">
-      {{-- <div class="input-group input-group-search">
+      <div class="input-group input-group-search">
         <input type="search" name="search" class="form-control" placeholder="Search">
         <span class="input-group-btn">
           <button class="btn"><i class="fa fa-search"></i></button>
-        </span><!-- input-group-btn -->
-      </div><!-- input-group --> --}}
+        </span>
+        <!-- input-group-btn -->
+      </div>
+      <!-- input-group -->
 
-      {{-- <label class="sidebar-label">Navigation</label> --}}
+      <label class="sidebar-label">Navigation</label>
       <div class="sl-sideleft-menu">
         <a href="{{ route('dashboard') }}" class="sl-menu-link active">
           <div class="sl-menu-item">
@@ -67,13 +74,13 @@
             <span class="menu-item-label">Cards &amp; Widgets</span>
           </div><!-- menu-item -->
         </a><!-- sl-menu-link -->
-        {{-- <a href="#" class="sl-menu-link">
+        <a href="#" class="sl-menu-link">
           <div class="sl-menu-item">
             <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
             <span class="menu-item-label">Charts</span>
             <i class="menu-item-arrow fa fa-angle-down"></i>
           </div><!-- menu-item -->
-        </a><!-- sl-menu-link --> --}}
+        </a><!-- sl-menu-link -->
         {{-- <ul class="sl-menu-sub nav flex-column">
           <li class="nav-item"><a href="chart-morris.html" class="nav-link">Morris Charts</a></li>
           <li class="nav-item"><a href="chart-flot.html" class="nav-link">Flot Charts</a></li>
