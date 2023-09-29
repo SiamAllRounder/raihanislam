5bef088b6800cf5a445d8ebedc03cb4c2384d963
siam <seum.bdit@gmail.com>
2023-09-29 11:25:15 +0600

    font awesome prob fix with cdn js on home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,8 @@
     <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/slick.css" />
     <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/floating-wpp.min.css" />
     <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/magnific-popup.css" />
-    <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/font-awesome.min.css" />
+    {{-- <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/font-awesome.min.css" /> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://mdfarukkhan.com/assets/landings/assets/css/style.css" />
 
     <link rel="stylesheet"
